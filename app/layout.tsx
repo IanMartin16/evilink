7417c0cf5506ef3fbadba01c4cf8d57df8b34551
ianmartin16 <imartindm20@gmail.com>
2026-03-04 21:07:53 -0600

    Design: new nexus design and icon v1.0.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NexusWidget from "./components/NexusWidget";
-import NexusWidgetShell from "./components/NexusWidgetShell";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
